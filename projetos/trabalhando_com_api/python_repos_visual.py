392d7a311f014b36f04b96a425535214cfdc5d81
IgorEustachio <igor.daniel.eustachio@gmail.com>
2026-03-16 21:53:53 -0300

    visualização gráfica da API

---
+++ projetos/trabalhando_com_api/python_repos_visual.py
@@ -0,0 +1,51 @@
+import requests 
+import plotly.express as px
+
+#cria uma chamada de api e verifica a resposta
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+
+print(f"Status code: {r.status_code}")
+
+#converte o objeto de resposta em um dicionário
+response_dict = r.json()
+print(f"resultados completos: {not response_dict['incomplete_results']}")
+
+# Processa as informações do repositório
+repo_dicts = response_dict['items']
+repo_names, stars, hover_texts = [], [], []
+print(f'repositorios retornados: {len(repo_dicts)}')
+
+"""#examina o primeiro repositorio
+repo_dict = repo_dicts[0]
+print(f"\n chaves: {len(repo_dict)}")
+for chave in sorted(repo_dict.keys()):
+    print(chave)
+#processa os resultados
+print(response_dict.keys())"""
+
+repo_links, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+# Transforma os nomes dos repositórios em links ativos
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    # Cria textos flutuantes
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+# Cria a visualização
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,hover_name=hover_texts)
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+fig.show()
